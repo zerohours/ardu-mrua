3b00a8bada2dab6d0a4c478ceb94066a5416538e
Jotam Jr. Trejo <jotamjr@gmail.com>
2010-11-26 00:37:06 -0600

    Se agrego el esquema del circuito

--- controlador/controlador.py
+++ controlador/controlador.py
@@ -1,4 +1,13 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0', 9600)
-while 1:
-    print ser.readline()
+try:
+    ser = serial.Serial('/dev/ttyUSB0', 9600)
+except:
+    print "Verifique que el dispositivo este conectado al computador"
+else:
+    comando = 'init'
+    while comando == 'init':
+        ser.write('4')
+        print ser.readline()
+        comando = raw_input("Desea seguir? ")
+    print "adios ..."
+    ser.close()
