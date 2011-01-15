7155b1980136633de56f10f5e22038235eb3edc8
Jotam Jr. Trejo <jotamjr@gmail.com>
2011-01-15 00:05:12 -0600

    Se agrego la capacidad para comunicarse con el servo

--- controlador/controlador.py
+++ controlador/controlador.py
@@ -1,21 +1,25 @@
 import serial, time
 
-class arduino_controlador:
+class arduino_controler:
 
     def __init__(self):
         self.serial=serial.Serial('/dev/ttyACM0', 9600, timeout=None)
 
-    def __capturarData(self):
+    def __getData(self):
 	return self.serial.readline().replace("\r\n","")
 
-    def __enviarData(self, serial_data):
-        while(self.capturarData()!="Que"):
+    def __sendData(self, serial_data):
+        while(self.__getData()!="wtf"):
 	    pass
 	self.serial.write(str(serial_data))
     
-    def leer_linea(self):
+    def read_line(self):
 	return self.serial.readline()
 
-    def desconectar(self):
+    def move_servo(self,grades):
+	self.serial.__sendData(2)
+	self.serial.__sendData(grades)
+
+    def disconnect(self):
         self.serial.close()
 	return True
